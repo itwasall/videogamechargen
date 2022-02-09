227bbd3010f6a8a679899df37c68dbbd5c684385
refrshrs <nthorgan@live.co.uk>
2022-02-09 15:23:27 +0000

    morrowind

---
+++ Morrowind/MorrowindSkill.py
@@ -0,0 +1,56 @@
+class MW_Skill:
+    def __init__(self, name, specialisation, mw_attribute):
+        self.name = name
+        self.specialisation = specialisation
+        self.mw_attribute = mw_attribute
+
+# Endurance Attribute Skills
+c,s,m,x = 'Combat', 'Stealth', 'Magic', 'Endurance'
+MWSkill_HeavyArmor = MW_Skill('Heavy Armour', c, x)
+MWSkill_MediumArmor = MW_Skill('Medium Armour', c, x)
+MWSkill_Spear = MW_Skill('Spear', c, x)
+
+# Strength Attribute Skills
+x = 'Strength'
+MWSkill_Acrobatics
+MWSkill_Armorer
+MWSkill_Axe
+MWSkill_BluntWeapon
+MWSkill_LongBlade
+
+# Agility Attribute Skills
+x = 'Agility'
+MWSkill_Block
+MWSkill_LightArmor
+MWSkill_Marksman
+MWSkill_Sneak
+
+# Speed Attribute Skills
+x = 'Speed'
+MWSkill_Athletics
+MWSkill_HandtoHand
+MWSkill_ShortBlade
+MWSkill_Unarmored
+
+# Personality Attribute Skills
+x = 'Personality'
+MWSkill_Illusion
+MWSkill_Mercantile
+MWSkill_Speechcraft
+
+# Intelligence Attribute Skills
+x = 'Intelligence'
+MWSkill_Alchemy
+MWSkill_Conjuration
+MWSkill_Enchant
+MWSkill_Security
+
+# Willpower Attribute Skills
+x = 'Willpower'
+MWSkill_Alteration
+MWSkill_Destruction
+MWSkill_Mysticism
+MWSkill_Restoration
+
+# Luck Attribute Skills
+# AHAHAHAAHAHAH
