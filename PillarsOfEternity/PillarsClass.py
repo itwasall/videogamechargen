2f4f66ba0df65ab084fea3017b0f5b690504e9df
refrshrs <nthorgan@live.co.uk>
2022-02-10 18:10:57 +0000

    Pillars class

--- PillarsOfEternity/PillarsClass.py
+++ PillarsOfEternity/PillarsClass.py
@@ -28,3 +28,25 @@ PClass_Priest = Pillars_Class('Priest', c_data['Priest'])
 PClass_Ranger = Pillars_Class('Ranger', c_data['Ranger'])
 PClass_Rogue = Pillars_Class('Rogue', c_data['Rogue'])
 PClass_Wizard = Pillars_Class('Wizard', c_data['Wizard'])
+
+def choice_class_exception_resolver(query):
+    if query == 'chanter':
+        # Choose two first level chant phrases and one first level invocation
+        pass
+    elif query == 'cipher':
+        # Choose two first level powers
+        pass
+    elif query == 'druid':
+        # Choose a spiritshift form
+        pass
+    elif query == 'paladin':
+        # Choose an order
+        pass
+    elif query == 'priest':
+        # Choose a deity
+        pass
+    elif query == 'ranger':
+        # Choose an animal companion
+        pass
+    elif query == 'wizard':
+        # Choose first four first level wizard spells
