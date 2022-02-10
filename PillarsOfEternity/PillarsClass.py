1c4f8b995acc4f5c931f9b456c597ecb940c9ddd
refrshrs <nthorgan@live.co.uk>
2022-02-10 18:10:07 +0000

    Pillars class

--- PillarsOfEternity/PillarsClass.py
+++ PillarsOfEternity/PillarsClass.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from yaml import safe_load
 
-class _Class:
-    def __init__(self, name):
-        self.name
+c_data = safe_load(open('PillarsOfEternity/data/Class.yml', 'rt'))['classes']
+
+class Pillars_Class:
+    def __init__(self, name, data):
+        self.name = name
+        self.endurance = data['endurance']
+        self.health = data['health']
+        self.accuracy = data['accuracy']
+        self.defenses = data['defenses']
+        self.skills = data['skills']
+        self.resource = data['resource']
 
     def __repr__(self):
         return self.name
 
-
+PClass_Barbarian = Pillars_Class('Barbarian', c_data['Barbarian'])
+PClass_Chanter = Pillars_Class('Chanter', c_data['Chanter'])
+PClass_Cipher = Pillars_Class('Cipher', c_data['Cipher'])
+PClass_Druid = Pillars_Class('Druid', c_data['Druid'])
+PClass_Fighter = Pillars_Class('Fighter', c_data['Fighter'])
+PClass_Monk = Pillars_Class('Monk', c_data['Monk'])
+PClass_Paladin = Pillars_Class('Paladin', c_data['Paladin'])
+PClass_Priest = Pillars_Class('Priest', c_data['Priest'])
+PClass_Ranger = Pillars_Class('Ranger', c_data['Ranger'])
+PClass_Rogue = Pillars_Class('Rogue', c_data['Rogue'])
+PClass_Wizard = Pillars_Class('Wizard', c_data['Wizard'])
